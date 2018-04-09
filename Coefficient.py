4b2f98538a48d378c0f624141bfd3534e0c82218
sangho-song <sangho_song@kmou.ac.kr>
2018-04-09 17:13:44 +0900

    형상계수 함수 추가
    방형계수
    중앙단면계수
    수선면적계수
    주형계수
    연직주형계수

--- Coefficient.py
+++ Coefficient.py
@@ -1,4 +1,27 @@
-#Coefficient
-class coefficient:
-    def __init__(self):
-        pass
+#Coefficient of Form
+class Coefficient:
+    def __init__(self, L, B, T, V):
+        self.L = L
+        self.B = B
+        self.T = T
+        self.V = V
+
+    #Block Coefficient
+    def Cb(self):
+        self.V / self.L*self.B*self.T
+
+    #Midship Section Coefficient
+    def Cm(self, Am):
+        Am / self.B*self.T
+
+    #Waterplane Coefficient
+    def Cw(self, Aw):
+        Aw / self.L * self.B
+
+    #Prismatic Coefficient
+    def Cp(self, Am):
+        self.V / self.L * Am
+
+    #Vertical Prismatic Coefficient
+    def Cvp(self, Vw, Aw):
+        Vw / Aw * self.T
